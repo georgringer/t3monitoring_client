2e9aecb007a6a1e4b05752c8a67e79026c3ca8c2
Georg Duemmler <gd@bergwerk.ag>
2016-05-09 17:14:27 +0200

    add avatar and email address

--- Classes/Provider/BackendUserProvider.php
+++ Classes/Provider/BackendUserProvider.php
@@ -11,6 +11,8 @@ namespace T3Monitor\T3monitoringClient\Provider;
 
 use TYPO3\CMS\Beuser\Domain\Model\BackendUser;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Service\ImageService;
 
 class BackendUserProvider implements DataProviderInterface
 {
@@ -28,20 +30,44 @@ class BackendUserProvider implements DataProviderInterface
     {
 
         $this->objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
-        $this->backendUserRepository = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Domain\\Repository\\BackendUserRepository');
+        $this->backendUserRepository = $this->objectManager->get('TYPO3\\CMS\\Beuser\\Domain\\Repository\\BackendUserRepository');
 
         /** @var BackendUser[] $backendUsers */
         $backendUsers = $this->backendUserRepository->findAll();
         foreach($backendUsers as $backendUser)
         {
-            $data['users']['backend'][] = array(
+            $backendUserData = array(
                 'userName' => $backendUser->getUserName(),
                 'realName' => $backendUser->getRealName(),
+                'emailAddress' => $backendUser->getEmail(),
                 'description' => $backendUser->getDescription(),
-                'lastLogin' => $backendUser->getLastLoginDateAndTime()
+                'lastLogin' => $backendUser->getLastLoginDateAndTime(),
+                'avatar' => ''
             );
+            if (GeneralUtility::compat_version('7.5')) {
+                $backendUserData['avatar'] = $this->getAvatarUrl($backendUser->getUid());
+            }
+            $data['users']['backend'][] = $backendUserData;
         }
-
         return $data;
+    }
+
+    protected function getAvatarUrl($beUserUid)
+    {
+        $fileRepository = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\FileRepository');
+        $fileObjects = $fileRepository->findByRelation('be_users', 'avatar', $beUserUid);
+        if(count($fileObjects) > 0)
+        {
+            $fileObject = $fileObjects[0];
+            $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+            $imageService = $objectManager->get(ImageService::class);
+            $processingInstructions = array(
+                'width' => '32c',
+                'height' => '32',
+            );
+            $processedImage = $imageService->applyProcessingInstructions($fileObject, $processingInstructions);
+            return $imageService->getImageUri($processedImage);
+        }
+        return NULL;
     }
 }
