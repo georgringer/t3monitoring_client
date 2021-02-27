6b17afdbf8355a18b553aa8b218d523c6b5a96d9
Klaus Hörmann-Engl <kho@world-direct.at>
2021-02-27 17:14:47 +0100

    [TASK] Unserialize task obj and add info to JSON

--- Classes/Provider/TaskProvider.php
+++ Classes/Provider/TaskProvider.php
@@ -11,6 +11,7 @@ namespace T3Monitor\T3monitoringClient\Provider;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Scheduler\Task\AbstractTask;
 
 /**
  * Class TaskProvider
@@ -37,8 +38,40 @@ class TaskProvider implements DataProviderInterface
             )
             ->execute();
         while ($row = $statement->fetch()) {
-            $data['tasks'][] = $row;
+            $data['tasks'][] = $this->unserializeTask($row);
         }
         return $data;
+    }
+
+    /**
+     * Unserialize a serialized task object. Fill some of the objects attributes
+     * into the $task array and return it.
+     *
+     * @param array<string> $row The task row
+     *
+     * @return array<string> The task array
+     */
+    private function unserializeTask($row)
+    {
+        /** @var \TYPO3\CMS\Scheduler\Task\AbstractTask|ProgressProviderInterface $task */
+        $task= unserialize($row['serialized_task_object']);
+        $class = get_class($task);
+        if ($class === \__PHP_Incomplete_Class::class && preg_match('/^O:[0-9]+:"(?P<classname>.+?)"/', $row['serialized_task_object'], $matches) === 1) {
+            $class = $matches['classname'];
+        }
+        unset($row['serialized_task_object']);
+        $row['class'] = $class;
+        $row['interval'] = $task->getExecution()->getInterval();
+        $row['cronCmd'] = $task->getExecution()->getCronCmd();
+        $row['multiple'] = intval($task->getExecution()->getMultiple());
+        if (!empty($row['interval']) || !empty($row['cronCmd'])) {
+            $row['type'] = AbstractTask::TYPE_RECURRING;
+            $row['frequency'] = $row['interval'] ?: $row['cronCmd'];
+        } else {
+            $row['type'] = AbstractTask::TYPE_SINGLE;
+            $row['frequency'] = '';
+        }
+        unset($row['cronCmd'], $row['interval']);
+        return $row;
     }
 }
